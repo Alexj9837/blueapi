7cf105be7eedaecf8b0435db614cc1bb2abd305e
Alexj9837 <Alexj9837@users.noreply.github.com>
2026-04-02 10:34:25 +0000

    deploy: 51fa29415e7482decfabdc7ddc863d12f94b4d54

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.12.5.dev8+gb4466c043',
+    VERSION: '1.12.6.dev4+g51fa29415',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
